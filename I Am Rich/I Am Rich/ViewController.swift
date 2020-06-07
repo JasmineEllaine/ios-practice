61de905dc22f9b98b3aa99731e158d6a8ac9ac06
Jasmine Banares <jban0004@student.monash.edu>
2020-06-07 18:08:15 +1000

    added projects and resource

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jasmine Banares on 25/5/20.
+//  Copyright © 2020 Jasmine Banares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
